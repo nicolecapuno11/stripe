d4b6070ec78cf71ddd69fb43999f577c0a36e6da
nicolefrankie11 <colecapuno@gmail.com>
2022-09-10 15:49:24 +0800

    first commit

---
+++ my-product.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'vendor/autoload.php';
+
+\Stripe\Stripe::setApiKey('sk_test_51LgIR2JblQF3jJqWBq5N7JyVWW5sWpclXIbHksjBywAJCBk1Be1d4qESvLxasnxzUIfIEute1DvJxgSozB8U4xfS00jDG9CjBx');
+
+header('Content-Type: application/json');
+
+$YOUR_DOMAIN = 'http://localhost:4242/public';
+
+$checkout_session = \Stripe\Checkout\Session::create([
+  'line_items' => [[
+    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+    'price' => 'price_1LgJsfJblQF3jJqWz4nOj6Zs',
+    'quantity' => 1,
+  ]],
+  'mode' => 'payment',
+  'success_url' => $YOUR_DOMAIN . '/success.html',
+  'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+]);
+
+header("HTTP/1.1 303 See Other");
+header("Location: " . $checkout_session->url);
--- stripe-checkout.php
+++ stripe-checkout.php
@@ -10,10 +10,10 @@ $product = $stripe->products->retrieve(
 	[]
 );
 $price = $stripe->prices->retrieve('price_1LgJsfJblQF3jJqWz4nOj6Zs',[]);
-echo '<pre>';
-var_dump($product);
-var_dump($price);
-echo '</pre>';
+#echo '<pre>';
+#var_dump($product);
+#var_dump($price);
+#echo '</pre>';
 ?><!DOCTYPE html>
 <html>
   <head>
@@ -21,20 +21,24 @@ echo '</pre>';
     <link rel="stylesheet" href="style.css">
     <script src="https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=fetch"></script>
     <script src="https://js.stripe.com/v3/"></script>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
-  <body>
+  <body style="background-color: #EEE3CB;">
     <section>
+      <div class="container text-center" style="background-color: #F9F9F9; padding-bottom: 161px">
       <div class="product">
-        <img src="<?php echo array_pop($product->images); ?>" alt="<?php echo $product->name; ?>" />
+        <img src="<?php echo array_pop($product->images); ?>" alt="<?php echo $product->name; ?>" style="width: 500px; float: right;"/>
         <div class="description">
-          <h3><?php echo $product->name; ?></h3>
-          <p><?php echo $product->description; ?></p>
-          <h5><?php echo strtoupper($price->currency); ?> <?php echo $price->unit_amount_decimal; ?></h5>
+          <h3 style="text-align: left; color: #AD8B73; font-size:46px; float: left; padding-top: 50px;"><?php echo $product->name; ?></h3>
+          <p style="text-align: left; padding-left: 10px; padding-right: 500px; padding-top: 10px;"><?php echo $product->description; ?></p>
+          <h5 style="text-align: left; margin-left: 50px; padding-top: 100px;"><?php echo strtoupper($price->currency); ?> <?php echo $price->unit_amount_decimal; ?></h5>
         </div>
       </div>
-      <form action="/create-checkout-session.php" method="POST">
+      <form action="/my-product.php" method="POST">
         <button type="submit" id="checkout-button">Checkout</button>
       </form>
+      </div>
     </section>
   </body>
 </html>
